8d67866b8509b379e0ef21460d89373ba0ae1c20
Elimu Brian Joshua <bryanjelimu@gmail.com>
2026-06-17 11:56:21 +0300

    updates
    updates

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('This is the About Page.');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
